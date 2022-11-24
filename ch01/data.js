841bee862f59a771c9f1fb977fa341f6b339f662
jihoon <ljhljh211@naver.com>
2022-11-24 18:08:41 +0900

    [ADD] 챕터1 실습코드 추가

---
+++ ch01/data.js
@@ -0,0 +1,26 @@
+const invoicies = {
+  "customer": "BigCo",
+  "performances": [
+    {
+      "playID": "hamlet",
+      "audience": 55
+    },
+    {
+      "playID": "as-like",
+      "audience": 35
+    },
+    {
+      "playID": "othello",
+      "audience": 40
+    }
+  ]
+};
+
+const plays = {
+  "hamlet": { "name": "Hamlet", "type": "tragedy" },
+  "as-like": { "name": "As You Like It", "type": "comedy" },
+  "othello": { "name": "Othello", "type": "tragedy" }
+}
+
+export {invoicies, plays}
+
