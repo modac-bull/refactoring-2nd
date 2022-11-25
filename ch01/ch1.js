216acc91cc0585dc7e16ff1105bdb5bcdc4d41e5
jihoon <ljhljh211@naver.com>
2022-11-25 18:21:51 +0900

    [MOD] 변수 인라인 추출하기

--- ch01/ch1.js
+++ ch01/ch1.js
@@ -2,6 +2,34 @@ import { invoicies, plays } from "./data.js";
 
 console.log(invoicies);
 
+// 1.4 함수 추출하기
+function amountFor(aPerformance) {
+  let result = 0; // 명확한 이름으로 변경
+  switch (playFor(aPerformance).type) { // play를 playFor()호출로 변경
+    case "tragedy":
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+    case "comedy":
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
+      }
+      result += 300 * aPerformance.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${playFor(aPerformance).type}`);  // play를 playFor()호출로 변경
+  }
+  return result;
+}
+
+// play 변수 제거하기 -> 임시 변수를 질의 함수로 바꾸기
+function playFor(aPerformance) {
+  return plays[aPerformance.playID]
+}
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -12,45 +40,22 @@ function statement(invoice, plays) {
     minimumFractionDigits: 2,
   }).format;
 
-  // 1.4 함수 추출하기
-  function amountFor(aPerformance, play) {
-    let result = 0; // 명확한 이름으로 변경
-    switch (play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
-    return result;
-  }
-
   console.log(invoice)
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    // const play = playFor(perf); // 우변을 함수로 추출 후 제거
     
-    let thisAmount = amountFor(perf, play);
+    // let thisAmount = amountFor(perf); // amountFor()에 함수 선언 바꾸기를 통해 play 매개변수 제거 + 필요없어진 playFor(perf) 매개변수 제거
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${
+    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
-    totalAmount += thisAmount;
+    totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
