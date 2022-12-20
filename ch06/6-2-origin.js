758c9dc187983409d6669fc97223fa7ad5f330c7
jihoon <ljhljh211@naver.com>
2022-12-20 17:04:33 +0900

    [MOD] 6.2 함수 인라인하기 - 실습 코드 추가

---
+++ ch06/6-2-origin.js
@@ -0,0 +1,8 @@
+function getRating(driver) {
+  return moreThanFiveLateDeliveries(driver) ? 2 : 1;
+}
+
+function moreThanFiveLateDeliveries(driver) {
+  return driver.numberOfLateDeliveries > 5;
+}
+
